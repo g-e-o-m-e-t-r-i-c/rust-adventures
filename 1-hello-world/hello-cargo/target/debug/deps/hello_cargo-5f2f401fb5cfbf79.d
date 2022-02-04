8e9535d2b3c986c91ae65addd82c30f1052ac63b
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-02-04 15:20:28 +0800

    removed target and Cargo.lock for the rest of you to try compiling

--- 1-hello-world/hello-cargo/target/debug/deps/hello_cargo-5f2f401fb5cfbf79.d
+++
@@ -1,5 +0,0 @@
-/home/geo/Desktop/rust-adventures/1-hello-world/hello-cargo/target/debug/deps/hello_cargo-5f2f401fb5cfbf79: src/main.rs
-
-/home/geo/Desktop/rust-adventures/1-hello-world/hello-cargo/target/debug/deps/hello_cargo-5f2f401fb5cfbf79.d: src/main.rs
-
-src/main.rs:
--- 1-hello-world/hello-cargo/target/debug/hello-cargo.d
+++
@@ -1,1 +0,0 @@
-/home/geo/Desktop/rust-adventures/1-hello-world/hello-cargo/target/debug/hello-cargo: /home/geo/Desktop/rust-adventures/1-hello-world/hello-cargo/src/main.rs
