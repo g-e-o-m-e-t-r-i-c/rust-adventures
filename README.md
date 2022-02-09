747ac882188d0d33145d5b781cd8e929359c98b3
g-e-o-m-e-t-r-i-c <79387351+g-e-o-m-e-t-r-i-c@users.noreply.github.com>
2022-02-10 07:14:53 +0800

    add(rbe): 1.2.2 - display

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ This repository consists of:
 - Tutorials I follow from the [book](https://doc.rust-lang.org/book)
 - My attempts at the [exercises](https://doc.rust-lang.org/stable/rust-by-example) (Rust By Example)
 
-There is also a short [`cargo` cheatsheet](https://github.com/g-e-o-m-e-t-r-i-c/rust-adventures/blob/master/cargo-help.md) to help myself (and you guys, maybe) out with the package manager!
+There is also a short [`cargo` cheat sheet](https://github.com/g-e-o-m-e-t-r-i-c/rust-adventures/blob/master/cargo-help.md) to help myself (and you guys, maybe) out with the package manager!
 
 ## Stuff I Use
 
--- book/3-programming-concepts/datatypes/src/main.rs
+++ book/3-programming-concepts/datatypes/src/main.rs
@@ -7,30 +7,30 @@ fn main() {
 	// 1. TYPE CASTING
 	// if using .parse() to convert string to integer, must include the type
 	//         ***
-	let guess: u32 = "42".parse().expect("Not a number!");
+	let _guess: u32 = "42".parse().expect("Not a number!");
 
 	// 2. DATATYPES
 	// int (i8/i16/i32/i64/i128 [signed int] / u8/u16/u32/u64/u128 [unsigned int])
-	let x = 2; // i32 (default)
-	let y: i128 = 173521635169351153; // big number, i128
+	let _x = 2; // i32 (default)
+	let _y: i128 = 173521635169351153; // big number, i128
 
 	// float (f32/f64)
-	let a = 2.0; // f64 (default)
-	let b: f32 = 3.0; // f32
+	let _a = 2.0; // f64 (default)
+	let _b: f32 = 3.0; // f32
 
 	// bool
-	let t = true;
-	let f: bool = false; // explicit
+	let _t = true;
+	let _f: bool = false; // explicit
 
 	// char (single quotes only, strings are in double quotes, no explicit type annotation)
-	let c = 'c';
-	let z = '#';
-	let heart_eyed_cat = '😻';
+	let _c = 'c';
+	let _z = '#';
+	let _heart_eyed_cat = '😻';
 
 	// tuples (cannot change size)
 	let tup: (i32, f64, u8) = (500, 6.4, 1); // use parens to surround a tuple
-	let (x, y, z) = tup; // destructuring
-										 // println!("tuple: {}", tup); // cannot directly print a tuple out
+	let (_x, y, _z) = tup; // destructuring
+											 // println!("tuple: {}", tup); // cannot directly print a tuple out
 	println!("second element of tuple: {}", y); // destructuring
 	println!("third element of tuple: {}", tup.2); // accessing using index notation
 
@@ -40,10 +40,10 @@ fn main() {
 	println!("first element of arr: {}", arr[0]);
 
 	// 3. OPERATIONS (https://doc.rust-lang.org/book/appendix-02-operators.html)
-	let sum = 5 + 10;
-	let diff = 95.5 - 4.3;
-	let prod: f64 = 4.0 * 30.0;
-	let quotient_raw = 56.7 / 32.2; // quotient (float) works with float operands only
-	let quotient_floored = 2 / 3; // floored quotient (integer) works with integer operands only
-	let modu = 43 % 5;
+	let _sum = 5 + 10;
+	let _diff = 95.5 - 4.3;
+	let _prod: f64 = 4.0 * 30.0;
+	let _quotient_raw = 56.7 / 32.2; // quotient (float) works with float operands only
+	let _quotient_floored = 2 / 3; // floored quotient (integer) works with integer operands only
+	let _modu = 43 % 5;
 }
